a97b58f4302bbb77e7fc3e49f1d1a856b33e4b14
Jyotirmay Barman <jyotirmay@developerzilla.com>
2024-01-08 17:04:07 +0530

    add: readme create: converter func add: repo meta data

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-Bare bone boilerplate to get started with Nodejs & Typescript quickly
+Convert tailwind css to vanilla css
+
+### Installation
+```bash
+npm install @jyotirmay/tailwind-to-css
+```
+
+### usage
+```js
+import { convert } from "@jyotirmay/tailwind-to-css";
+
+let html = `
+    <div class="text-gray-500 dark:bg-red-500 my-container">
+        <p class="inline">Hello world</p>
+    </div> 
+`;
+
+(async()=>{
+    const res = await convert(html)
+    console.log(res)
+
+    /**
+    Output:
+        {
+            html: 
+            `
+                <html>
+                <head>
+                    <link rel="stylesheet" href="./output.css" />
+                </head>
+                <body>
+                    <div class="VzxbQ9x92_use1WNuDg0y my-container">
+                    <p class="AeplxTuJZjGFMK173hyC9">Hello world</p>
+                    </div>
+                </body>
+                </html>
+            `,
+            css: 
+            `
+                .VzxbQ9x92_use1WNuDg0y {
+                    --tw-text-opacity: 1;
+                    color: rgb(107 114 128 / var(--tw-text-opacity));
+                }
+                :is(.dark .VzxbQ9x92_use1WNuDg0y) {
+                    --tw-bg-opacity: 1;
+                    background-color: rgb(239 68 68 / var(--tw-bg-opacity));
+                }
+                .AeplxTuJZjGFMK173hyC9 {
+                    display: inline;
+                }
+            `
+        }
+     */
+})()
+
+
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log("Hello world");
+import { nanoid } from "nanoid";
+import { isValidTailwindClass } from "@jyotirmay/tailwind-class-validator";
+import { load } from "cheerio";
+
+export const convert = async (
+  inputHtml: string,
+  classNamePrefix: string = ""
+) => {
+  const $ = load(inputHtml);
+  $("head").append('<link rel="stylesheet" href="./output.css">');
+
+  let css = ``;
+  let html = ``;
+
+  let body = $("body *");
+  for (let i = 0; i < body.length; i++) {
+    const classes = $(body[i]).attr("class");
+
+    if (classes) {
+      const classList = classes.split(" ");
+      let tailwindClasses = "";
+      let customClasses = "";
+
+      for (let j = 0; j < classList.length; j++) {
+        let current = classList[j],
+          valid;
+        let special = current.includes(":");
+        if (special) {
+          let parts = current.split(":");
+          valid = await isValidTailwindClass(parts[parts.length - 1]);
+        } else {
+          valid = await isValidTailwindClass(current);
+        }
+        if (valid.success) tailwindClasses += current + " ";
+        else if (!special) customClasses += current + " ";
+      }
+
+      const newClassName = classNamePrefix + nanoid();
+
+      let generated = await isValidTailwindClass(tailwindClasses, {
+        outputClass: newClassName,
+        plugins: {
+          typography: true,
+        },
+      });
+      if (generated.success) css += generated.css + "\n";
+      $(body[i]).attr("class", newClassName + " " + customClasses);
+    }
+  }
+  
+  html = $.html();
+
+  return {
+    html,
+    css,
+  };
+};
